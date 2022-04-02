dead7339aeeb982c620cdaacf2d36653df32ed80
CristianCaro915 <c.caro@uniandes.edu.co>
2022-04-01 20:12:00 -0700

    Update Acividad y proyecto, metodos y consola en prueba

--- Project01DPOO/src/project1/modelo/Actividad.java
+++ Project01DPOO/src/project1/modelo/Actividad.java
@@ -1,21 +1,24 @@
 package project1.modelo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Actividad {
 	//Atributos
+	public final static SimpleDateFormat formato=new SimpleDateFormat("dd/HH/mm" );
 	private String titulo;
 	private String descripcion;
 	private String tipo;
-	private String horaInicio;
-	private String horaFin;
+	private Date horaInicio;
+	private Date horaFin;
 	private Participante creador;
 	//Constructor
-	public Actividad(String titulo, String descripcion, String tipo, String horaI, String horaF) 
+	public Actividad(String titulo, String descripcion, String tipo) 
 	{
 		this.titulo=titulo;
 		this.descripcion=descripcion;
 		this.tipo=tipo;
-		horaInicio=horaI;
-		horaFin=horaF;
 	}
 	//Req funcionales
 	
@@ -31,5 +34,32 @@ public class Actividad {
 	public String getDescripcion() 
 	{
 		return this.descripcion;
+	}
+	public Date setFechaInicial(String fechaI) throws ParseException 
+	{
+		this.horaInicio=formato.parse(fechaI);
+		return horaInicio;
+	}
+	public Date setFechaFinal(String fechaF) throws ParseException 
+	{
+		this.horaFin=formato.parse(fechaF);
+		return horaFin;
+	}
+	public String getTitle() 
+	{
+		return this.titulo;
+	}
+	@SuppressWarnings("deprecation") //manejo de error extraño
+	public int calcularDemora(Date fecha1, Date fecha2) 
+	{
+		int calc=0, f1_d,f1_h,f1_m,f2_d,f2_h,f2_m;
+		f1_d=fecha1.getDay()*1440; //1440 minutos en un día
+		f1_h=fecha1.getHours()*60; //60 minutos en una hora
+		f1_m=fecha1.getMinutes();
+		f2_d=fecha2.getDay()*1440;
+		f2_h=fecha2.getHours()*60;
+		f2_m=fecha2.getMinutes();
+		calc=f1_d+f1_h+f1_m+f2_d+f2_h+f2_m;
+		return calc;
 	}
 }
